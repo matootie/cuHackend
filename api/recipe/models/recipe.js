ecf1f13642b11be1a6f84de2e8cc3149a0ebb271
Ben Durston <bengdurston@gmail.com>
2021-01-30 20:22:37 -0500

    Initial commit

---
+++ api/recipe/models/recipe.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const axios = require("axios").default;
+axios.defaults.baseURL = 'http://localhost:7700'
+// axios.defaults.baseURL = process.env.BASE_URL;
+// axios.defaults.headers.common['Authorization'] = process.env.AUTH_TOKEN;
+
+module.exports = {
+  lifecycles: {
+    async afterCreate(result, data) {
+      const document = {};
+      document.id = result.id;
+      document.title = result.name;
+      document.quantities = result.ingredients;
+      document.tags = result.tags.map(x => x.value);
+      document.ingredients = Object.keys(result.ingredients);
+
+      console.log('Recipe has been created. Here is the document:', document);
+
+      try {
+        await axios.post('/indexes/recipes/documents', [ document ]);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async afterUpdate(result, params, data) {
+      const document = {};
+      document.id = result.id;
+      document.title = result.name;
+      document.quantities = result.ingredients;
+      document.tags = result.tags.map(x => x.value);
+      document.ingredients = Object.keys(result.ingredients);
+
+      console.log("Recipe has been updated. Here is the document:", document);
+
+      await axios.post('/indexes/recipes/documents', [ document ]);
+    },
+    async afterDelete(result, params) {
+      const document = {};
+      document.id = result.id;
+      document.title = result.name;
+
+      console.log("Recipe has been deleted. Here is the document:", document);
+
+      await axios.delete(`/indexes/recipes/documents/${result.id}`);
+    }
+  }
+};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3215809d-667b-44ea-8c71-641354db996d'
+};
